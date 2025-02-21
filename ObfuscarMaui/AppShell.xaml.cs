0ed5128ce0077644fb757fb8be116e203b1e07c3
Estivenson Ortega <estivenson.ortega@softwareone.com>
2025-02-21 16:51:50 -0500

    Project creation

---
+++ ObfuscarMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ObfuscarMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ObfuscarMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ObfuscarMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
